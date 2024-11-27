66ff020128afdfe68b444dc979b1b8c552e8c39c
excallaver <43446801+excallaver@users.noreply.github.com>
2024-11-26 19:22:08 -0500

    Add files via upload

---
+++ MetallurgyFrugalWorkspace.cs
@@ -0,0 +1,52 @@
+﻿
+
+namespace Eco.Mods.TechTree
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.Linq;
+    using Eco.Gameplay.Components;
+    using Eco.Gameplay.Items;
+    using Eco.Gameplay.Players;
+    using Eco.Gameplay.Skills;
+    using Eco.Mods.TechTree;
+    using Eco.Shared.Items;
+    using Eco.Shared.Localization;
+    using Eco.Shared.Serialization;
+    using Eco.Shared.Utils;
+    using Eco.Shared.View;
+
+    [Serialized]
+    [LocDisplayName("Frugal Workspace: Metallurgy")]
+    [LocDescription("Lowers the tier requirement of related tables by 0.2.(Only applies to claimed workstations)")]
+    public partial class MetallurgyFrugalWorkspaceTalentGroup : TalentGroup
+    {
+
+        public MetallurgyFrugalWorkspaceTalentGroup()
+        {
+            Talents = new Type[]
+            {
+
+            typeof(MetallurgyFrugalReqTalent),
+
+
+            };
+            this.OwningSkill = typeof(MetallurgySkill);
+            this.Level = 6;
+        }
+    }
+
+
+        [Serialized]
+        public partial class MetallurgyFrugalReqTalent : FrugalWorkspaceTalent
+        {
+            public override bool Base { get { return false; } }
+            public override Type TalentGroupType { get { return typeof(global::Eco.Mods.TechTree.MetallurgyFrugalWorkspaceTalentGroup); } }
+            public MetallurgyFrugalReqTalent()
+            {
+                this.Value = -0.2f;
+            }
+        }
+
+    }
